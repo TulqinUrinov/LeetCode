a29b6812e164d84ffad352f38e5f08fb300be267
MSI <tulqinurinov005@gmail.com>
2025-11-14 15:17:13 +0500

    der

--- 42/SQL/SELECT/perefered.py
+++ 42/SQL/SELECT/perefered.py
@@ -43,7 +43,6 @@ Kutilgan natija:
 +---------+
 """
 
-
 class SQL:
 
     def prefer(self):
---
+++ 42/algorithm/array/highest.py
@@ -0,0 +1,8 @@
+a = [1,4,6,9,2,5,69,21]
+largest = a[0]
+
+for i in a:
+    if i > largest:
+        largest = i
+
+print(largest)
